3d21560085db1e4fe6786e3638d41f14716f85b1
qizl <q@qizl.cn>
2014-11-18 16:49:13 +0800

    -

---
+++ XmlSerializer/XmlSerialize.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace XmlSerializerLibrary
+{
+    public class XmlSerialize
+    {
+        public XmlDocument Xml { get; protected set; }
+
+        public XmlDocument Serialize(object obj)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+                ns.Add("", ""); // 去除默认命名空间
+
+                XmlSerializer xs = new XmlSerializer(obj.GetType());
+                xs.Serialize(ms, obj, ns);
+
+                ms.Seek(0, SeekOrigin.Begin);
+
+                this.Xml = new XmlDocument();
+                this.Xml.Load(ms);
+            }
+
+            return this.Xml;
+        }
+
+        public T Deserialize<T>(XmlDocument xml) where T : class
+        {
+            object obj;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                xml.Save(ms);
+                ms.Seek(0, SeekOrigin.Begin);
+                using (XmlReader reader = XmlReader.Create(ms))
+                {
+                    XmlSerializer xs = new XmlSerializer(typeof(T));
+                    obj = xs.Deserialize(reader);
+                }
+            }
+            return obj as T;
+        }
+    }
+}
---
+++ XmlSerializerTest/Program.cs
@@ -0,0 +1,48 @@
+﻿using SharpSerializerLibrary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace XmlSerializerTest
+{
+    /// <summary>
+    /// 序列化方法测试类
+    /// </summary>
+    public class TestClass
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public List<int> Values { get; set; }
+        public List<float> Values2 { get; set; }
+        public byte[] Values3 { get; set; }
+        public Dictionary<string, int> Value4 { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass t = new TestClass()
+            {
+                ID = Guid.NewGuid(),
+                Name = "睡觉了快递费",
+                Number = 123,
+                Values = new List<int>() { 1, 2, 3, 4, 5 },
+                Values2 = new List<float>() { 1, 2, 3, 4, 5 },
+                Values3 = new byte[] { 1, 2, 3, 4, 5 },
+                Value4 = new Dictionary<string, int>() { { "1", 1 }, { "2", 2 } }
+            };
+
+            SharpSerializer s = new SharpSerializer();
+            string path = Path.Combine(Environment.CurrentDirectory, "test.xml");
+
+            s.Serialize(t, path);
+
+            var t1 = s.Deserialize(path);
+        }
+    }
+}
