bb1539df0a7bd23f2b20369a8c55b76c25f812ce
qizl <q@qizl.cn>
2015-03-23 15:05:39 +0800

    -

---
+++ SharpSerializer.Tests/TestSharpSerializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Com.EnjoyCodes.SharpSerializer;
+using System.Diagnostics;
+
+namespace SharpSerializerTests
+{
+    [TestClass]
+    public class TestSharpSerializer
+    {
+        public class Test
+        {
+            public Guid ID { get; set; }
+            public string Name { get; set; }
+            public DateTime Time { get; set; }
+        }
+
+        [TestMethod]
+        public void TestSerialize()
+        {
+            List<Test> datas = new List<Test>();
+            for (int i = 0; i < 100; i++)
+                datas.Add(new Test()
+                {
+                    ID = Guid.NewGuid(),
+                    Name = "Test" + Guid.NewGuid(),
+                    Time = DateTime.Now
+                });
+            SharpSerializer serializer = new SharpSerializer();
+            string str = serializer.Serialize(datas);
+            Debug.WriteLine(str);
+
+            Assert.IsTrue(!string.IsNullOrEmpty(str));
+        }
+    }
+}
